3f26cd30753387efda0a5b1ef1a8b0dff646e890
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2025-03-25 16:43:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-import feedparser, time
- 
+import feedparser
+import time
+import re
+
 URL = "https://gamxong.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
- 
-markdown_text = """
-## ✅ Latest Blog Post
- 
-"""  # list of blog posts will be appended here
- 
+
+# 새로운 블로그 글 목록 만들기
+markdown_text = "## ✅ Latest Blog Post\n\n"
 for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx > MAX_POST:
+    if idx >= MAX_POST:
         break
-    else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-        
-f = open("README.md", mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+    feed_date = feed['published_parsed']
+    markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+
+# README 파일 불러오기
+with open("README.md", "r", encoding="utf-8") as f:
+    readme_contents = f.read()
+
+# 기존 블로그 섹션 대체
+updated_readme = re.sub(
+    r"(<!-- BLOG-POST-START -->)(.*?)(<!-- BLOG-POST-END -->)",
+    f"\\1\n{markdown_text}\n\\3",
+    readme_contents,
+    flags=re.DOTALL
+)
+
+# README 다시 저장
+with open("README.md", "w", encoding="utf-8") as f:
+    f.write(updated_readme)
